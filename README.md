6a86b6acd8cdb7e7860f96fbfe0d31fdadd9f89f
noahmenzi <noah.menzi@webwirkung.ch>
2022-02-22 20:14:52 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mocoapp-massasign-projects-script
+# Mass Assign a new project leader to multiple Moco projects
 This python script helps to change the project leader of multiple projects in the Moco App.
+
+## How it works
+Download this script or clone this repository and replace the {{DOMAIN}} part in the URL and paste your API KEY, which you can get from your Moco Profil. This script uses the Moco API, the documentation can be found: https://github.com/hundertzehn/mocoapp-api-docs/blob/master/README.md#moco-api-documentation
+
+After replacing the mentioned values above, you are asked to enter the ID of the current project leader. The script fetches all assigned projects of this person and asks for the ID of the new project leader. After entering, it replaces the project leader for all the previously fetched projects and confirms it.
---
+++ change_projects.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# importing the module
+import json
+import requests
+
+# adjust to your moco domain
+URL = "https://{{domain}}.mocoapp.com/api/v1/projects/"
+ 
+ # Headers, replace YOUR_API_KEY
+HEADERS = {
+    "Content-Type": "multipart/form-data;",
+    "Authorization": "Bearer YOUR_API_KEY"
+}
+
+
+print("This Python script helps you to change the project leader of a lot of Moco projects at once.")
+old_id = input("Please enter the id of the project leader which should be replaced by a new one: ")
+print("Great. This is the list of projects assigned to this project leader:")
+
+querystring = {"leader_id":old_id, "include_archived":"false"}
+projects = requests.request("GET", URL, headers=HEADERS, params=querystring)
+data = projects.json()
+
+print("The following projects will be changed:")
+for i in data:
+    print(i['id'], i['name'])
+
+new_id = input("Please enter the id of the new project leader. This new leader will be assign to all projects of the old project leader: ")
+
+for i in data:
+    project_url = URL + str(i['id'])
+    print(project_url)
+    response = requests.request("PUT", project_url, data={'leader_id' : '933641692'}, headers=HEADERS)
+    print(response.status_code)
+
+print("Success!")
